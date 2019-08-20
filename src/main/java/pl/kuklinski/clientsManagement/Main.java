a1544e7d4f8193c18143d51b2f78f65710b6553c
Riczard <riczardkiller@gmail.com>
2019-08-20 16:53:03 +0200

    implement Main class to show main Pane, change compiler source and target to 1.8

--- src/main/java/pl/kuklinski/clientsManagement/Main.java
+++ src/main/java/pl/kuklinski/clientsManagement/Main.java
@@ -1,16 +1,26 @@
 package pl.kuklinski.clientsManagement;
 
 import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
+import pl.kuklinski.clientsManagement.utils.FXMLUtils;
 
 public class Main extends Application {
 
-    public static void main(String[] args) {
+    private static final String BORDER_PANE_MAIN_FXML = "/fxml/BorderPaneMain.fxml";
 
+    public static void main(String[] args) {
+        launch(args);
     }
 
     @Override
-    public void start(Stage primaryStage) throws Exception {
+    public void start(Stage primaryStage) {
 
+        Pane borderPane = FXMLUtils.fxmlLoader(BORDER_PANE_MAIN_FXML);
+        Scene scene = new Scene(borderPane);
+        primaryStage.setScene(scene);
+        primaryStage.setTitle(FXMLUtils.getResourceBundle().getString("title.application"));
+        primaryStage.show();
     }
 }
