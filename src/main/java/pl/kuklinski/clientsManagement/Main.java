56ef7e49ac04f085d8b1b38e3286a7e355a8ad9a
Riczard <riczardkiller@gmail.com>
2019-08-20 16:11:01 +0200

    add main class and add github link to pom

---
+++ src/main/java/pl/kuklinski/clientsManagement/Main.java
@@ -0,0 +1,16 @@
+package pl.kuklinski.clientsManagement;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    public static void main(String[] args) {
+
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+    }
+}
