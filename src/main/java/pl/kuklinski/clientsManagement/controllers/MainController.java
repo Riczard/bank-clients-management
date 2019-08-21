d2952993e56dc819e49b0683ca413fcf64aef733
Riczard <riczardkiller@gmail.com>
2019-08-21 16:35:03 +0200

    add method AlwaysOnTop and closeApp

--- src/main/java/pl/kuklinski/clientsManagement/controllers/MainController.java
+++ src/main/java/pl/kuklinski/clientsManagement/controllers/MainController.java
@@ -1,8 +1,16 @@
 package pl.kuklinski.clientsManagement.controllers;
 
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.CheckMenuItem;
 import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+import pl.kuklinski.clientsManagement.utils.DialogUtils;
 import pl.kuklinski.clientsManagement.utils.FXMLUtils;
+
+import java.util.Optional;
 
 public class MainController {
 
@@ -19,5 +27,19 @@ public class MainController {
 
     public void setCenter(String fxmlPath) {
         borderPane.setCenter(FXMLUtils.fxmlLoader(fxmlPath));
+    }
+
+    public void setAlwaysOnTop(ActionEvent actionEvent) {
+        Stage stage = (Stage) borderPane.getScene().getWindow();
+        boolean value = ((CheckMenuItem) actionEvent.getSource()).isSelected();
+        stage.setAlwaysOnTop(value);
+    }
+
+    public void closeApp() {
+        Optional<ButtonType> result = DialogUtils.confirmationDialog();
+        if (result.get() == ButtonType.OK) {
+            Platform.exit();
+            System.exit(0);
+        }
     }
 }
