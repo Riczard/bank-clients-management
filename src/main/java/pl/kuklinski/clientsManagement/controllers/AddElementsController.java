10f1104eb43ac65704b13861b4183a25fbafa984
Riczard <riczardkiller@gmail.com>
2019-08-21 19:14:21 +0200

    implement Controller and add onAction methods

---
+++ src/main/java/pl/kuklinski/clientsManagement/controllers/AddElementsController.java
@@ -0,0 +1,15 @@
+package pl.kuklinski.clientsManagement.controllers;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.TextField;
+
+public class AddElementsController {
+    public TextField textFieldAccStatus;
+    public TextField textFieldAdviser;
+
+    public void addAccStatus() {
+    }
+
+    public void addAdviser() {
+    }
+}
