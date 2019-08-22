842df5458f94c2fb456d3c41c6aff10bff764c04
Riczard <riczardkiller@gmail.com>
2019-08-22 17:13:46 +0200

    split field into two fields

--- src/main/java/pl/kuklinski/clientsManagement/modelFX/AdviserFX.java
+++ src/main/java/pl/kuklinski/clientsManagement/modelFX/AdviserFX.java
@@ -5,17 +5,30 @@ import javafx.beans.property.StringProperty;
 
 public class AdviserFX {
 
-    private StringProperty adviserName = new SimpleStringProperty();
+    private StringProperty name = new SimpleStringProperty();
+    private StringProperty surname = new SimpleStringProperty();
 
-    public String getAdviserName() {
-        return adviserName.get();
+    public String getName() {
+        return name.get();
     }
 
-    public StringProperty adviserNameProperty() {
-        return adviserName;
+    public StringProperty nameProperty() {
+        return name;
     }
 
-    public void setAdviserName(String adviserName) {
-        this.adviserName.set(adviserName);
+    public void setName(String name) {
+        this.name.set(name);
+    }
+
+    public String getSurname() {
+        return surname.get();
+    }
+
+    public StringProperty surnameProperty() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname.set(surname);
     }
 }
