46fbede446958043f34bf3ed495d1d86581d82a8
Riczard <riczardkiller@gmail.com>
2019-08-23 14:41:38 +0200

    implement AdviserController

--- src/main/java/pl/kuklinski/clientsManagement/controllers/AddAdvisersController.java
+++ src/main/java/pl/kuklinski/clientsManagement/controllers/AddAdvisersController.java
@@ -2,15 +2,50 @@ package pl.kuklinski.clientsManagement.controllers;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.MenuItem;
 import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import pl.kuklinski.clientsManagement.database.dao.AdviserDao;
+import pl.kuklinski.clientsManagement.modelFX.AdviserFX;
+import pl.kuklinski.clientsManagement.modelFX.AdviserModel;
 
 public class AddAdvisersController {
 
     @FXML
-    private TableColumn nameAdviserColumn;
+    private TableView<AdviserFX> adviserTableView;
     @FXML
-    private TableColumn surnameAdviserColumn;
+    private TextField adviserSurnameField;
+    @FXML
+    private TextField adviserNameField;
+    @FXML
+    private MenuItem deleteMenuItem;
+    @FXML
+    private TableColumn<AdviserFX, String> nameAdviserColumn;
+    @FXML
+    private TableColumn<AdviserFX, String> surnameAdviserColumn;
 
-    public void addAdviser(ActionEvent actionEvent) {
+    private AdviserModel adviserModel;
+
+    public void initialize() {
+        this.adviserModel = new AdviserModel();
+        this.adviserModel.init();
+        bindingTableView();
+    }
+
+    private void bindingTableView() {
+        this.adviserTableView.setItems(this.adviserModel.getAdviserFXObservableList());
+    }
+
+
+    public void addAdviser() {
+        this.adviserModel.saveAdviserInDataBase();
+        this.adviserNameField.clear();
+        this.adviserSurnameField.clear();
+    }
+
+
+    public void deleteAdviser() {
+        this.adviserModel.deleteAdviserInDb();
     }
 }
