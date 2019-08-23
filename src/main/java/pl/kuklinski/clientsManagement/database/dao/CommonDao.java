d3ce010b486ec087e75c58b71e003c950cb73470
Riczard <riczardkiller@gmail.com>
2019-08-23 15:07:06 +0200

    implement CommonDao

--- src/main/java/pl/kuklinski/clientsManagement/database/dao/CommonDao.java
+++ src/main/java/pl/kuklinski/clientsManagement/database/dao/CommonDao.java
@@ -1,4 +1,57 @@
 package pl.kuklinski.clientsManagement.database.dao;
 
+import pl.kuklinski.clientsManagement.database.dbutils.DbManager;
+import pl.kuklinski.clientsManagement.database.models.BaseModel;
+
+import javax.persistence.EntityManager;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
+import java.util.stream.Stream;
+
 public class CommonDao {
+
+    private final EntityManager entityManager;
+
+    public CommonDao() {
+        this.entityManager = DbManager.getEntityManager();
+    }
+
+    public <T extends BaseModel> void create(T t) {
+        entityManager.getTransaction().begin();
+        entityManager.persist(t);
+        entityManager.getTransaction().commit();
+    }
+
+    public <T extends BaseModel> void update(T t) {
+        entityManager.getTransaction().begin();
+        entityManager.merge(t);
+        entityManager.getTransaction().commit();
+    }
+
+    public <T extends BaseModel> Stream<T> queryForAll(Class<T> cls) {
+        CriteriaQuery<T> criteria = getCriteriaQuery(cls);
+        Root<T> root = criteria.from(cls);
+        criteria.select(root);
+        return entityManager.createQuery(criteria).getResultStream();
+    }
+
+    public <T, I extends BaseModel> T findById(Class<T> cls, long id) {
+        return entityManager.find(cls, id);
+    }
+
+    private <T extends BaseModel> CriteriaQuery<T> getCriteriaQuery(Class<T> cls) {
+        CriteriaBuilder builder = entityManager.getCriteriaBuilder();
+        return builder.createQuery(cls);
+    }
+
+    public <T extends BaseModel> void delete(T t) {
+        entityManager.getTransaction().begin();
+        entityManager.remove(entityManager.contains(t) ? t : entityManager.merge(t));
+        entityManager.getTransaction().commit();
+    }
+
+    public void closeConnection() {
+        entityManager.close();
+    }
 }
