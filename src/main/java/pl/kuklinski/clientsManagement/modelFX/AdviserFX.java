d9a163fd2c43ee33827b6606f1cf4f21f77e8fd3
Riczard <riczardkiller@gmail.com>
2019-08-23 14:30:27 +0200

    add id and method toString

--- src/main/java/pl/kuklinski/clientsManagement/modelFX/AdviserFX.java
+++ src/main/java/pl/kuklinski/clientsManagement/modelFX/AdviserFX.java
@@ -1,10 +1,13 @@
 package pl.kuklinski.clientsManagement.modelFX;
 
+import javafx.beans.property.LongProperty;
+import javafx.beans.property.SimpleLongProperty;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.property.StringProperty;
 
 public class AdviserFX {
 
+    private LongProperty id = new SimpleLongProperty();
     private StringProperty name = new SimpleStringProperty();
     private StringProperty surname = new SimpleStringProperty();
 
@@ -30,5 +33,22 @@ public class AdviserFX {
 
     public void setSurname(String surname) {
         this.surname.set(surname);
+    }
+
+    public long getId() {
+        return id.get();
+    }
+
+    public LongProperty idProperty() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id.set(id);
+    }
+
+    @Override
+    public String toString() {
+        return name.get() + " " + surname.get();
     }
 }
