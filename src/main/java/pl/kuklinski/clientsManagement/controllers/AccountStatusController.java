0e8bd78ca4d346ba192c67c7c8d2205729aad68d
Riczard <riczardkiller@gmail.com>
2019-08-23 16:30:47 +0200

    implement AccountStatusController

---
+++ src/main/java/pl/kuklinski/clientsManagement/controllers/AccountStatusController.java
@@ -0,0 +1,56 @@
+package pl.kuklinski.clientsManagement.controllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.TextField;
+import pl.kuklinski.clientsManagement.modelFX.AccountStatusFX;
+import pl.kuklinski.clientsManagement.modelFX.AccountStatusModel;
+
+public class AccountStatusController {
+
+    @FXML
+    private MenuItem deleteStatus;
+    @FXML
+    private ListView<AccountStatusFX> statusListView;
+    @FXML
+    private Button addButton;
+    @FXML
+    private TextField statusField;
+
+    private AccountStatusModel accStatusModel;
+
+    public void initialize() {
+        this.accStatusModel = new AccountStatusModel();
+        this.accStatusModel.init();
+        bindings();
+        bindingsListView();
+    }
+
+    private void bindings() {
+        this.accStatusModel.accountStatusFXObjectPropertyy().get().accountStatusProperty().bind(this.statusField.textProperty());
+        this.addButton.disableProperty().bind(this.statusField.textProperty().isEmpty());
+        this.deleteStatus.disableProperty().bind(this.statusListView.getSelectionModel().selectedItemProperty().isNull());
+    }
+
+    private void bindingsListView() {
+        this.statusListView.setItems(this.accStatusModel.getAccountStatusFXObservableList());
+        this.statusListView.getSelectionModel().selectedItemProperty().addListener((observable, oldValue, newValue) -> {
+            this.accStatusModel.setAccountStatusFXObjectPropertyEdit(newValue);
+        });
+    }
+
+
+    @FXML
+    public void addAccStatus() {
+        this.accStatusModel.saveStatusInDataBase();
+        this.statusField.clear();
+    }
+
+
+    public void deleteStatusOnAction() {
+        this.accStatusModel.deleteStatusInDataBase();
+    }
+}
