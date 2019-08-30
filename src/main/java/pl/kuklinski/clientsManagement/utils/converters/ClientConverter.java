511005d6bb4b0217462c6237e31fc68b93e66b05
Riczard <riczardkiller@gmail.com>
2019-08-30 14:08:02 +0200

    implement method convertToClient

--- src/main/java/pl/kuklinski/clientsManagement/utils/converters/ClientConverter.java
+++ src/main/java/pl/kuklinski/clientsManagement/utils/converters/ClientConverter.java
@@ -19,11 +19,34 @@ public class ClientConverter {
         clientFX.setLastContactDate(client.getLastContactDate());
         clientFX.setVerificationDate(client.getVerificationDate());
         if(client.getStatus() != null) {
-            clientFX.setAccountState(AccountStatusConverter.convertToAccStatusFX(client.getStatus()));
+            clientFX.setAccountStatus(AccountStatusConverter.convertToAccStatusFX(client.getStatus()));
         }
         if(client.getAdviser() != null) {
             clientFX.setAdviser(AdviserConverter.convertToAdviserFx(client.getAdviser()));
         }
         return clientFX;
+    }
+
+    public static Client convertToClient(ClientFX clientFX) {
+        Client client = new Client();
+        client.setId(clientFX.getId());
+        client.setName(clientFX.getName());
+        client.setSurname(clientFX.getSurname());
+        client.setPesel(clientFX.getPesel());
+        client.setPhone(clientFX.getPhone());
+        client.setCity(clientFX.getCity());
+        client.setIncome(clientFX.getIncome());
+        client.setRelation(clientFX.getRelation());
+        client.setClickAmount(clientFX.getClickAmount());
+        client.setComment(clientFX.getComment());
+        client.setLastContactDate(clientFX.getLastContactDate());
+        client.setVerificationDate(clientFX.getVerificationDate());
+        if(clientFX.getAccountStatus() != null) {
+            client.setStatus(AccountStatusConverter.convertToAccStatus(clientFX.getAccountStatus()));
+        }
+        if(client.getAdviser() != null) {
+            clientFX.setAdviser(AdviserConverter.convertToAdviserFx(client.getAdviser()));
+        }
+        return client;
     }
 }
