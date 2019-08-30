bbe7caf9d520040fdec4096fc9c888783c1486d0
Riczard <riczardkiller@gmail.com>
2019-08-30 13:42:29 +0200

    ad show popup method

--- src/main/java/pl/kuklinski/clientsManagement/utils/FXMLUtils.java
+++ src/main/java/pl/kuklinski/clientsManagement/utils/FXMLUtils.java
@@ -1,8 +1,12 @@
 package pl.kuklinski.clientsManagement.utils;
 
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.layout.Pane;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.util.ResourceBundle;
 
 public class FXMLUtils {
@@ -22,6 +26,20 @@ public class FXMLUtils {
         FXMLLoader loader = new FXMLLoader(FXMLUtils.class.getResource(fxmlPath));
         loader.setResources(getResourceBundle());
         return loader;
+    }
+
+    public static void showPopUp(String fxmlPath) {
+        FXMLLoader loader = getLoader(fxmlPath);
+        Scene scene = null;
+        try {
+            scene = new Scene(loader.load());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        Stage stage = new Stage();
+        stage.setScene(scene);
+        stage.initModality(Modality.APPLICATION_MODAL);
+        stage.showAndWait();
     }
 
     public static ResourceBundle getResourceBundle() {
