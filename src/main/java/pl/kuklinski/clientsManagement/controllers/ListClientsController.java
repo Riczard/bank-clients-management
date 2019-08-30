7a6790c79e1c146987c15915a48bc44b7e9630b6
Riczard <riczardkiller@gmail.com>
2019-08-30 21:22:12 +0200

    implement combobox in tableview

--- src/main/java/pl/kuklinski/clientsManagement/controllers/ListClientsController.java
+++ src/main/java/pl/kuklinski/clientsManagement/controllers/ListClientsController.java
@@ -3,6 +3,8 @@ package pl.kuklinski.clientsManagement.controllers;
 import javafx.fxml.FXML;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.cell.ComboBoxTableCell;
+import javafx.scene.control.cell.PropertyValueFactory;
 import pl.kuklinski.clientsManagement.modelFX.AdviserFX;
 import pl.kuklinski.clientsManagement.modelFX.ClientFX;
 import pl.kuklinski.clientsManagement.modelFX.ListClientsModel;
@@ -45,12 +47,21 @@ public class ListClientsController {
 
     private void setCells() {
         this.clientsTableView.setItems(this.listClientsModel.getClientFXObservableList());
-        this.nameColumn.setCellValueFactory(cellData -> cellData.getValue().nameProperty());
+        this.nameColumn.setCellValueFactory(new PropertyValueFactory<>("name"));
         this.surnameColumn.setCellValueFactory(cellData -> cellData.getValue().surnameProperty());
         this.peselColumn.setCellValueFactory(cellData -> cellData.getValue().peselProperty());
         this.clickAmountColumn.setCellValueFactory(cellData -> cellData.getValue().clickAmountProperty());
         this.phoneColumn.setCellValueFactory(cellData -> cellData.getValue().phoneProperty());
         this.cityColumn.setCellValueFactory(cellData -> cellData.getValue().cityProperty());
+        this.incomeColumn.setCellValueFactory(cellData -> cellData.getValue().incomeProperty());
+
+        this.adviserColumn.setCellValueFactory(new PropertyValueFactory<>("adviser"));
+        this.adviserColumn.setCellFactory(ComboBoxTableCell.forTableColumn(this.listClientsModel.getAdviserFXObservableList()));
+        this.adviserColumn.setOnEditCommit(event -> {
+            event.getRowValue().setAdviser(event.getNewValue());
+            listClientsModel.updateInDataBase(event.getRowValue());
+        });
+
     }
 
     public void addClient() {
