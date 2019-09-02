93cd3079d7055bd1b9abaeadd50dbf48026e8992
Riczard <riczardkiller@gmail.com>
2019-09-02 15:51:42 +0200

    implement importClients method

--- src/main/java/pl/kuklinski/clientsManagement/controllers/TopMenuButtonsController.java
+++ src/main/java/pl/kuklinski/clientsManagement/controllers/TopMenuButtonsController.java
@@ -8,6 +8,7 @@ public class TopMenuButtonsController {
     private static final String LIST_CLIENTS_FXML = "/fxml/ListClients.fxml";
     private static final String ADD_ADVISERS_FXML = "/fxml/AddAdvisers.fxml";
     private static final String ADD_ACCOUNT_STATUS_FXML = "/fxml/AddAccountStatus.fxml";
+    private static final String IMPORT_PANE_FXML = "/fxml/ImportPane.fxml";
 
     private MainController mainController;
 
@@ -19,10 +20,6 @@ public class TopMenuButtonsController {
 
     @FXML
     public void searchClient(ActionEvent actionEvent) {
-    }
-
-    @FXML
-    public void importFile(ActionEvent actionEvent) {
     }
 
     @FXML
@@ -41,5 +38,9 @@ public class TopMenuButtonsController {
 
     public void addAccStatus() {
         mainController.setCenter(ADD_ACCOUNT_STATUS_FXML);
+    }
+
+    public void importClients() {
+        mainController.setCenter(IMPORT_PANE_FXML);
     }
 }
