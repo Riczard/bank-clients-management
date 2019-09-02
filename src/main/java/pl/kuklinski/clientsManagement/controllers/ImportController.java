bc30830b0dd21e0fe7f6c82163b460ff59243833
Riczard <riczardkiller@gmail.com>
2019-09-02 16:38:57 +0200

    add importController with fields and empty method

---
+++ src/main/java/pl/kuklinski/clientsManagement/controllers/ImportController.java
@@ -0,0 +1,30 @@
+package pl.kuklinski.clientsManagement.controllers;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class ImportController {
+    public Button importButton;
+    public TextField statusClient;
+    public TextField adviserClient;
+    public TextField verificationDateClient;
+    public TextField lastContactDateClient;
+    public TextField commentClient;
+    public TextField clickAmountClient;
+    public TextField relationClient;
+    public TextField peselClient;
+    public TextField incomeClient;
+    public TextField phoneClient;
+    public TextField cityClient;
+    public TextField surnameClient;
+    public TextField nameClient;
+    public Label filePath;
+
+    public void chooseFile(ActionEvent event) {
+    }
+
+    public void importData(ActionEvent event) {
+    }
+}
