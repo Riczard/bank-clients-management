825a3bbd9e3ba13ae0035decc4e4d200b654f3c4
Riczard <riczardkiller@gmail.com>
2019-09-03 21:39:23 +0200

    implement method findByNameAndSurname

--- src/main/java/pl/kuklinski/clientsManagement/database/dao/AdviserDao.java
+++ src/main/java/pl/kuklinski/clientsManagement/database/dao/AdviserDao.java
@@ -1,8 +1,19 @@
 package pl.kuklinski.clientsManagement.database.dao;
 
+import pl.kuklinski.clientsManagement.database.models.Adviser;
+
+import javax.persistence.Query;
+
 public class AdviserDao extends CommonDao{
 
     public AdviserDao() {
         super();
+    }
+
+    public Adviser findByNameAndSurname(String[] adviserData) {
+        Query query = getEntityManager().createQuery("select a from Adviser a where a.name like :name AND  a.surname like :surname");
+        query.setParameter("name", adviserData[0]);
+        query.setParameter("surname", adviserData[1]);
+        return (Adviser) query.getResultStream().findFirst().orElse(null);
     }
 }
