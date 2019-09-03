6365f820b9614f98eb54cf0880c4bd96df72aa58
Riczard <riczardkiller@gmail.com>
2019-09-03 21:39:39 +0200

    implement method findByPesel

--- src/main/java/pl/kuklinski/clientsManagement/database/dao/ClientDao.java
+++ src/main/java/pl/kuklinski/clientsManagement/database/dao/ClientDao.java
@@ -1,4 +1,19 @@
 package pl.kuklinski.clientsManagement.database.dao;
 
-public class ClientDao extends CommonDao{
+import pl.kuklinski.clientsManagement.database.models.Client;
+
+import javax.persistence.Query;
+import java.util.Optional;
+
+public class ClientDao extends CommonDao {
+
+    public ClientDao() {
+        super();
+    }
+
+    public Client findByPesel(String pesel) {
+        Query query = getEntityManager().createQuery("select c from Client c where c.pesel like :pesel");
+        query.setParameter("pesel", pesel.trim());
+        return (Client) query.getResultList().stream().findFirst().orElse(null);
+    }
 }
