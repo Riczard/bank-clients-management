572ef78c4e6f274c232f1df635ccaf92782fa29c
Riczard <riczardkiller@gmail.com>
2019-09-04 21:13:42 +0200

    implement OfferStatusFX class

--- src/main/java/pl/kuklinski/clientsManagement/javaFX/modelFX/OfferStatusFX.java
+++ src/main/java/pl/kuklinski/clientsManagement/javaFX/modelFX/OfferStatusFX.java
@@ -1,4 +1,36 @@
 package pl.kuklinski.clientsManagement.javaFX.modelFX;
 
+import javafx.beans.property.LongProperty;
+import javafx.beans.property.SimpleLongProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+
 public class OfferStatusFX {
+
+    private LongProperty id = new SimpleLongProperty();
+    private StringProperty title = new SimpleStringProperty();
+
+    public long getId() {
+        return id.get();
+    }
+
+    public LongProperty idProperty() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id.set(id);
+    }
+
+    public String getTitle() {
+        return title.get();
+    }
+
+    public StringProperty titleProperty() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title.set(title);
+    }
 }
