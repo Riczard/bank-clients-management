6bda90aece70480ab2333b9092aa044da52b85b9
Riczard <riczardkiller@gmail.com>
2019-09-04 20:52:41 +0200

    implement new setItemsToComboBox

--- src/main/java/pl/kuklinski/clientsManagement/controllers/ClientController.java
+++ src/main/java/pl/kuklinski/clientsManagement/controllers/ClientController.java
@@ -1,41 +1,45 @@
 package pl.kuklinski.clientsManagement.controllers;
 
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
 import javafx.stage.Stage;
-import pl.kuklinski.clientsManagement.modelFX.AccountStatusFX;
-import pl.kuklinski.clientsManagement.modelFX.AdviserFX;
-import pl.kuklinski.clientsManagement.modelFX.ClientModel;
+import pl.kuklinski.clientsManagement.javaFX.model.ClientModel;
+import pl.kuklinski.clientsManagement.javaFX.modelFX.AdviserFX;
+import pl.kuklinski.clientsManagement.javaFX.modelFX.ContactStatusFX;
+import pl.kuklinski.clientsManagement.javaFX.modelFX.OfferStatusFX;
+import pl.kuklinski.clientsManagement.javaFX.modelFX.RelationFX;
 
 public class ClientController {
 
     @FXML
+    private Button addButton;
+
+    @FXML
     private TextField nameField;
     @FXML
-    private Button addButton;
-    @FXML
-    private TextArea commentArea;
-    @FXML
-    private ComboBox<AdviserFX> adviserComboBox;
-    @FXML
-    private DatePicker lastContactDate;
-    @FXML
-    private DatePicker verificationDate;
-    @FXML
-    private ComboBox<AccountStatusFX> accStatusComboBox;
-    @FXML
-    private TextField relationField;
-    @FXML
-    private TextField phoneField;
-    @FXML
-    private TextField incomeField;
-    @FXML
-    private TextField cityField;
+    private TextField surnameField;
     @FXML
     private TextField peselField;
     @FXML
-    private TextField surnameField;
+    private TextField phoneField;
+    @FXML
+    private ComboBox<OfferStatusFX> statusOfferField;
+    @FXML
+    private ComboBox<RelationFX> relationField;
+    @FXML
+    private ComboBox<ContactStatusFX> contactStatusField;
+    @FXML
+    private DatePicker lastContactField;
+    @FXML
+    private DatePicker plannedDateField;
+    @FXML
+    private TextArea commentField;
+    @FXML
+    private TextField incomeTypeField;
+    @FXML
+    private TextField cityField;
+    @FXML
+    private ComboBox<AdviserFX> adviserField;
 
     private ClientModel clientModel;
 
@@ -43,28 +47,32 @@ public class ClientController {
     public void initialize() {
         this.clientModel = new ClientModel();
         this.clientModel.init();
-
-        this.adviserComboBox.setItems(this.clientModel.getAdviserFXObservableList());
-        this.accStatusComboBox.setItems(this.clientModel.getAccountStatusFXObservableList());
-        
+        setItemsToComboBox();
         bindings();
-        validation();
+//        validation();
+    }
+
+    private void setItemsToComboBox() {
+        this.adviserField.setItems(this.clientModel.getAdviserFXES());
+        this.statusOfferField.setItems(this.clientModel.getOfferStatusFXES());
+        this.contactStatusField.setItems(this.clientModel.getContactStatusFXES());
+        this.relationField.setItems(this.clientModel.getRelationFXES());
     }
 
     private void bindings() {
         this.clientModel.getClientFXObjectProperty();
-        this.accStatusComboBox.valueProperty().bindBidirectional(this.clientModel.getClientFXObjectProperty().accountStatusProperty());
-        this.adviserComboBox.valueProperty().bindBidirectional(this.clientModel.getClientFXObjectProperty().adviserProperty());
+//        this.accStatusComboBox.valueProperty().bindBidirectional(this.clientModel.getClientFXObjectProperty().accountStatusProperty());
+//        this.adviserComboBox.valueProperty().bindBidirectional(this.clientModel.getClientFXObjectProperty().adviserProperty());
         this.nameField.textProperty().bindBidirectional(this.clientModel.getClientFXObjectProperty().nameProperty());
         this.surnameField.textProperty().bindBidirectional(this.clientModel.getClientFXObjectProperty().surnameProperty());
-        this.commentArea.textProperty().bindBidirectional(this.clientModel.getClientFXObjectProperty().commentProperty());
+//        this.commentArea.textProperty().bindBidirectional(this.clientModel.getClientFXObjectProperty().commentProperty());
         this.peselField.textProperty().bindBidirectional(this.clientModel.getClientFXObjectProperty().peselProperty());
-        this.relationField.textProperty().bindBidirectional(this.clientModel.getClientFXObjectProperty().relationProperty());
-        this.lastContactDate.valueProperty().bindBidirectional(this.clientModel.getClientFXObjectProperty().lastContactDateProperty());
-        this.verificationDate.valueProperty().bindBidirectional(this.clientModel.getClientFXObjectProperty().verificationDateProperty());
+//        this.relationField.textProperty().bindBidirectional(this.clientModel.getClientFXObjectProperty().relationProperty());
+//        this.lastContactDate.valueProperty().bindBidirectional(this.clientModel.getClientFXObjectProperty().lastContactDateProperty());
+//        this.verificationDate.valueProperty().bindBidirectional(this.clientModel.getClientFXObjectProperty().verificationDateProperty());
         this.phoneField.textProperty().bindBidirectional(this.clientModel.getClientFXObjectProperty().phoneProperty());
-        this.incomeField.textProperty().bindBidirectional(this.clientModel.getClientFXObjectProperty().incomeProperty());
-        this.cityField.textProperty().bindBidirectional(this.clientModel.getClientFXObjectProperty().cityProperty());
+//        this.incomeField.textProperty().bindBidirectional(this.clientModel.getClientFXObjectProperty().incomeTypeProperty());
+//        this.cityField.textProperty().bindBidirectional(this.clientModel.getClientFXObjectProperty().cityProperty());
     }
 
     private void validation() {
