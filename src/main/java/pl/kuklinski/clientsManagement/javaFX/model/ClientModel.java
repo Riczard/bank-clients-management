d9aefc21bca9d0cb4f510afdf37b12e590af63b9
Riczard <riczardkiller@gmail.com>
2019-09-04 21:03:25 +0200

    implement method inifOfferStatusList

--- src/main/java/pl/kuklinski/clientsManagement/javaFX/model/ClientModel.java
+++ src/main/java/pl/kuklinski/clientsManagement/javaFX/model/ClientModel.java
@@ -7,9 +7,11 @@ import javafx.collections.ObservableList;
 import pl.kuklinski.clientsManagement.database.dao.AdviserDao;
 import pl.kuklinski.clientsManagement.database.dao.ClientDao;
 import pl.kuklinski.clientsManagement.database.dao.ContactStatusDao;
+import pl.kuklinski.clientsManagement.database.dao.OfferStatusDao;
 import pl.kuklinski.clientsManagement.database.models.Adviser;
 import pl.kuklinski.clientsManagement.database.models.Client;
 import pl.kuklinski.clientsManagement.database.models.ContactStatus;
+import pl.kuklinski.clientsManagement.database.models.OfferStatus;
 import pl.kuklinski.clientsManagement.javaFX.modelFX.*;
 import pl.kuklinski.clientsManagement.utils.converters.AccountStatusConverter;
 import pl.kuklinski.clientsManagement.utils.converters.AdviserConverter;
@@ -55,6 +57,14 @@ public class ClientModel {
     }
 
     private void initOfferStatusList() {
+        offerStatusFXES.clear();
+        OfferStatusDao statusDao = new OfferStatusDao();
+        Stream<OfferStatus> offerStatusStream = statusDao.queryForAll(OfferStatus.class);
+        offerStatusStream.forEach(status -> {
+            OfferStatusFX offerStatusFX = OfferStatusConverter.convertToOfferStatusFX(status);
+            offerStatusFXES.add(offerStatusFX);
+        });
+        statusDao.closeConnection();
     }
 
     public void saveClientInDataBase() {
