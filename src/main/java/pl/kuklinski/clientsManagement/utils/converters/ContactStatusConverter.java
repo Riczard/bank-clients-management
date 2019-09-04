44c1cf6d7cc251382ed4905014c87c4f4fc95d8e
Riczard <riczardkiller@gmail.com>
2019-09-04 21:58:57 +0200

    rename Converter

---
+++ src/main/java/pl/kuklinski/clientsManagement/utils/converters/ContactStatusConverter.java
@@ -0,0 +1,21 @@
+package pl.kuklinski.clientsManagement.utils.converters;
+
+import pl.kuklinski.clientsManagement.database.models.ContactStatus;
+import pl.kuklinski.clientsManagement.javaFX.modelFX.ContactStatusFX;
+
+public class ContactStatusConverter {
+
+    public static ContactStatusFX convertToAccStatusFX(ContactStatus contactStatus) {
+        ContactStatusFX contactStatusFX = new ContactStatusFX();
+        contactStatusFX.setId(contactStatus.getId());
+        contactStatusFX.setContactStatus(contactStatus.getTitle());
+        return contactStatusFX;
+    }
+
+    public static ContactStatus convertToAccStatus(ContactStatusFX contactStatusFX) {
+        ContactStatus contactStatus = new ContactStatus();
+        contactStatus.setId(contactStatusFX.getId());
+        contactStatus.setTitle(contactStatusFX.getContactStatus());
+        return contactStatus;
+    }
+}
