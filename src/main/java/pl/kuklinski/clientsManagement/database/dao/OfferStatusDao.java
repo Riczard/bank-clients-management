38fb80d66bfa01816f495e971a6b35e035ff2556
Riczard <riczardkiller@gmail.com>
2019-09-04 20:39:10 +0200

    add dao for new entity

---
+++ src/main/java/pl/kuklinski/clientsManagement/database/dao/OfferStatusDao.java
@@ -0,0 +1,8 @@
+package pl.kuklinski.clientsManagement.database.dao;
+
+public class OfferStatusDao extends CommonDao{
+
+    public OfferStatusDao() {
+        super();
+    }
+}
---
+++ src/main/java/pl/kuklinski/clientsManagement/database/dao/RelationDao.java
@@ -0,0 +1,8 @@
+package pl.kuklinski.clientsManagement.database.dao;
+
+public class RelationDao extends CommonDao {
+
+    public RelationDao() {
+        super();
+    }
+}
