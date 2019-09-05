984a2c3886c2d806b21f59cc10eca84cf6afb530
Riczard <riczardkiller@gmail.com>
2019-09-05 16:59:11 +0200

    add delimiter parameter

--- src/main/java/pl/kuklinski/clientsManagement/utils/CSVUtils.java
+++ src/main/java/pl/kuklinski/clientsManagement/utils/CSVUtils.java
@@ -1,22 +1,22 @@
 package pl.kuklinski.clientsManagement.utils;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
 public class CSVUtils {
 
-    public static List<String[]> getDataFromCSV(File file) {
-        String line = "";
-        String csvSplitBy = ";";
+    public static List<String[]> getDataFromCSV(File file, String delimiter) {
+        String line;
         List<String[]> data = new ArrayList<>();
 
         try (BufferedReader br = new BufferedReader(new FileReader(file))) {
-
             while ((line = br.readLine()) != null) {
-                data.add(line.split(csvSplitBy));
+                data.add(line.split(delimiter));
             }
-
         } catch (IOException e) {
             e.printStackTrace();
         }
