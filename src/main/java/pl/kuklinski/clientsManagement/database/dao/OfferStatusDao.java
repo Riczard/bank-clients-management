a6812b73c9ab3e6bd652810cd0c49d9813238069
Riczard <riczardkiller@gmail.com>
2019-09-05 20:51:36 +0200

    implement method findByName

--- src/main/java/pl/kuklinski/clientsManagement/database/dao/OfferStatusDao.java
+++ src/main/java/pl/kuklinski/clientsManagement/database/dao/OfferStatusDao.java
@@ -1,8 +1,18 @@
 package pl.kuklinski.clientsManagement.database.dao;
 
+import pl.kuklinski.clientsManagement.database.models.OfferStatus;
+
+import javax.persistence.Query;
+
 public class OfferStatusDao extends CommonDao{
 
     public OfferStatusDao() {
         super();
+    }
+
+    public OfferStatus findByName(String name) {
+        Query query = getEntityManager().createQuery("select o from OfferStatus o where o.title=:name");
+        query.setParameter("name", name);
+        return (OfferStatus) query.getResultStream().findFirst().orElse(null);
     }
 }
