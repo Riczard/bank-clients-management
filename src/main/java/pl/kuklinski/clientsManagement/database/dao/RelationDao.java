4d157e83466a459eb65607287b12149163e2257f
Riczard <riczardkiller@gmail.com>
2019-09-05 21:02:58 +0200

    implement method findByName

--- src/main/java/pl/kuklinski/clientsManagement/database/dao/RelationDao.java
+++ src/main/java/pl/kuklinski/clientsManagement/database/dao/RelationDao.java
@@ -1,8 +1,18 @@
 package pl.kuklinski.clientsManagement.database.dao;
 
+import pl.kuklinski.clientsManagement.database.models.Relation;
+
+import javax.persistence.Query;
+
 public class RelationDao extends CommonDao {
 
     public RelationDao() {
         super();
+    }
+
+    public Relation findByName(String clientData) {
+        Query query = getEntityManager().createQuery("select r from Relation r where r.title like :relation");
+        query.setParameter("relation", clientData);
+        return (Relation) query.getResultStream().findFirst().orElse(null);
     }
 }
