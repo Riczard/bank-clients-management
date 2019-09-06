9b8bb5f0d62e465b9bf775ec70df69749c78890c
Riczard <riczardkiller@gmail.com>
2019-09-06 17:44:23 +0200

    add SearchController with fields and empty methods

---
+++ src/main/java/pl/kuklinski/clientsManagement/controllers/SearchController.java
@@ -0,0 +1,29 @@
+package pl.kuklinski.clientsManagement.controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+
+public class SearchController {
+
+    @FXML
+    private ComboBox creditTypeCombobox;
+    @FXML
+    private Label directoryPath;
+    @FXML
+    private CheckBox createTxtCheckbox;
+    @FXML
+    private CheckBox addToDbCheckbox;
+    @FXML
+    private Button searchButton;
+
+    @FXML
+    public void chooseFolder() {
+    }
+
+    @FXML
+    public void searchCredits() {
+    }
+}
