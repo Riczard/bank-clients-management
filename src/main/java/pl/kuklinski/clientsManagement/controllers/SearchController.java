54130f7935e83cc6e76926e5e4352f9811f7b1a6
Riczard <riczardkiller@gmail.com>
2019-09-06 19:55:21 +0200

    implement validation method

--- src/main/java/pl/kuklinski/clientsManagement/controllers/SearchController.java
+++ src/main/java/pl/kuklinski/clientsManagement/controllers/SearchController.java
@@ -5,11 +5,14 @@ import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
+import pl.kuklinski.clientsManagement.javaFX.model.SearchModel;
+import pl.kuklinski.clientsManagement.javaFX.modelFX.CreditFX;
+import pl.kuklinski.clientsManagement.utils.FXMLUtils;
 
 public class SearchController {
 
     @FXML
-    private ComboBox creditTypeCombobox;
+    private ComboBox<CreditFX> creditTypeCombobox;
     @FXML
     private Label directoryPath;
     @FXML
@@ -18,6 +21,37 @@ public class SearchController {
     private CheckBox addToDbCheckbox;
     @FXML
     private Button searchButton;
+
+    private SearchModel searchModel;
+
+    @FXML
+    public void initialize() {
+        this.searchModel = new SearchModel();
+        initComboBox();
+        validation();
+    }
+
+    private void initComboBox() {
+        CreditFX preapprovedCredit = new CreditFX();
+        preapprovedCredit.setTitle(FXMLUtils.getResourceBundle().getString("creditPreapproved.search"));
+        preapprovedCredit.setType("preapproved");
+        creditTypeCombobox.getItems().add(preapprovedCredit);
+
+        CreditFX consolidationCredit = new CreditFX();
+        consolidationCredit.setTitle(FXMLUtils.getResourceBundle().getString("creditConsolidation.search"));
+        consolidationCredit.setType("consolidation");
+        creditTypeCombobox.getItems().add(consolidationCredit);
+
+        CreditFX allTypes = new CreditFX();
+        allTypes.setTitle(FXMLUtils.getResourceBundle().getString("creditAll"));
+        allTypes.setType("all");
+        creditTypeCombobox.getItems().add(allTypes);
+    }
+
+    private void validation() {
+        this.searchButton.disableProperty().bind(this.directoryPath.textProperty().isEmpty()
+                .or((createTxtCheckbox.selectedProperty()).not()).and(addToDbCheckbox.selectedProperty().not()));
+    }
 
     @FXML
     public void chooseFolder() {
