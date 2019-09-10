4b076860c4e43f566e62645d9a4564284ef86b98
Riczard <riczardkiller@gmail.com>
2019-09-10 14:16:28 +0200

    remove unused methods

--- src/main/java/pl/kuklinski/clientsManagement/controllers/TopMenuButtonsController.java
+++ src/main/java/pl/kuklinski/clientsManagement/controllers/TopMenuButtonsController.java
@@ -6,13 +6,11 @@ import javafx.fxml.FXML;
 public class TopMenuButtonsController {
 
     private static final String LIST_CLIENTS_FXML = "/fxml/ListClients.fxml";
-    private static final String ADD_ADVISERS_FXML = "/fxml/AddAdvisers.fxml";
-    private static final String ADD_ACCOUNT_STATUS_FXML = "/fxml/AddContactStatus.fxml";
     private static final String IMPORT_PANE_FXML = "/fxml/ImportPane.fxml";
     private static final String SEARCH_PANE_FXML = "/fxml/SearchPane.fxml";
+    private static final String RESOURCES_BUTTONS_FXML = "/fxml/ResourcesButtons.fxml";
 
     private MainController mainController;
-
 
     @FXML
     public void showClients() {
@@ -29,23 +27,17 @@ public class TopMenuButtonsController {
     }
 
     @FXML
-    public void setMainController(MainController mainController) {
-        this.mainController = mainController;
-    }
-
-
-    public void addAdvisers() {
-        mainController.setCenter(ADD_ADVISERS_FXML);
-    }
-
-    public void addAccStatus() {
-        mainController.setCenter(ADD_ACCOUNT_STATUS_FXML);
-    }
-
     public void importClients() {
         mainController.setCenter(IMPORT_PANE_FXML);
     }
 
-    public void openResources(ActionEvent event) {
+    @FXML
+    public void openResources() {
+        this.mainController.setCenter(RESOURCES_BUTTONS_FXML);
+    }
+
+    @FXML
+    public void setMainController(MainController mainController) {
+        this.mainController = mainController;
     }
 }
