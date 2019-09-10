1a7e0061d74cb047cc541053a1bd10ac8576ca10
Riczard <riczardkiller@gmail.com>
2019-09-10 14:51:45 +0200

    refactor

--- src/main/java/pl/kuklinski/clientsManagement/javaFX/model/ContactStatusModel.java
+++ src/main/java/pl/kuklinski/clientsManagement/javaFX/model/ContactStatusModel.java
@@ -13,20 +13,20 @@ import java.util.stream.Stream;
 
 public class ContactStatusModel {
 
-    private ObjectProperty<ContactStatusFX> accountStatusFXObjectProperty = new SimpleObjectProperty<>(new ContactStatusFX());
-    private ObjectProperty<ContactStatusFX> accountStatusFXObjectPropertyEdit = new SimpleObjectProperty<>(new ContactStatusFX());
+    private ObjectProperty<ContactStatusFX> contactStatusFXObjectProperty = new SimpleObjectProperty<>(new ContactStatusFX());
+    private ObjectProperty<ContactStatusFX> contactStatusFXObjectPropertyEdit = new SimpleObjectProperty<>(new ContactStatusFX());
 
     private ObservableList<ContactStatusFX> contactStatusFXObservableList = FXCollections.observableArrayList();
 
     public void init() {
         this.contactStatusFXObservableList.clear();
-        addAccStatusToObservableList();
+        addContactStatusToObsList();
     }
 
-    private void addAccStatusToObservableList() {
+    private void addContactStatusToObsList() {
         ContactStatusDao contactStatusDao = new ContactStatusDao();
-        Stream<ContactStatus> accStatusStream = contactStatusDao.queryForAll(ContactStatus.class);
-        accStatusStream.forEach(status -> {
+        Stream<ContactStatus> contactStatusStream = contactStatusDao.queryForAll(ContactStatus.class);
+        contactStatusStream.forEach(status -> {
             ContactStatusFX contactStatusFX = ContactStatusConverter.convertToAccStatusFX(status);
             this.contactStatusFXObservableList.add(contactStatusFX);
         });
@@ -35,47 +35,47 @@ public class ContactStatusModel {
 
     public void saveStatusInDataBase() {
         ContactStatusDao contactStatusDao = new ContactStatusDao();
-        ContactStatus contactStatus = ContactStatusConverter.convertToContactStatus(this.getAccountStatusFXObjectProperty());
+        ContactStatus contactStatus = ContactStatusConverter.convertToContactStatus(this.getContactStatusFXObjectProperty());
         contactStatusDao.create(contactStatus);
         init();
     }
 
     public void deleteStatusInDataBase() {
         ContactStatusDao contactStatusDao = new ContactStatusDao();
-        contactStatusDao.delete(ContactStatusConverter.convertToContactStatus(this.getAccountStatusFXObjectPropertyEdit()));
+        contactStatusDao.delete(ContactStatusConverter.convertToContactStatus(this.getContactStatusFXObjectPropertyEdit()));
         contactStatusDao.closeConnection();
         init();
     }
 
     public void updateStatusInDB() {
         ContactStatusDao contactStatusDao = new ContactStatusDao();
-        contactStatusDao.update(ContactStatusConverter.convertToContactStatus(this.getAccountStatusFXObjectPropertyEdit()));
+        contactStatusDao.update(ContactStatusConverter.convertToContactStatus(this.getContactStatusFXObjectPropertyEdit()));
         contactStatusDao.closeConnection();
         init();
     }
 
-    public ContactStatusFX getAccountStatusFXObjectProperty() {
-        return accountStatusFXObjectProperty.get();
+    public ContactStatusFX getContactStatusFXObjectProperty() {
+        return contactStatusFXObjectProperty.get();
     }
 
-    public ObjectProperty<ContactStatusFX> accountStatusFXObjectPropertyy() {
-        return accountStatusFXObjectProperty;
+    public ObjectProperty<ContactStatusFX> contactStatusFXObjectPropertyy() {
+        return contactStatusFXObjectProperty;
     }
 
-    public void setAccountStatusFXObjectProperty(ContactStatusFX contactStatusFXObjectProperty) {
-        this.accountStatusFXObjectProperty.set(contactStatusFXObjectProperty);
+    public void setContactStatusFXObjectProperty(ContactStatusFX contactStatusFXObjectProperty) {
+        this.contactStatusFXObjectProperty.set(contactStatusFXObjectProperty);
     }
 
-    public ContactStatusFX getAccountStatusFXObjectPropertyEdit() {
-        return accountStatusFXObjectPropertyEdit.get();
+    public ContactStatusFX getContactStatusFXObjectPropertyEdit() {
+        return contactStatusFXObjectPropertyEdit.get();
     }
 
-    public ObjectProperty<ContactStatusFX> accountStatusFXObjectPropertyEdit() {
-        return accountStatusFXObjectPropertyEdit;
+    public ObjectProperty<ContactStatusFX> contactStatusFXObjectPropertyEdit() {
+        return contactStatusFXObjectPropertyEdit;
     }
 
-    public void setAccountStatusFXObjectPropertyEdit(ContactStatusFX contactStatusFXObjectPropertyEdit) {
-        this.accountStatusFXObjectPropertyEdit.set(contactStatusFXObjectPropertyEdit);
+    public void setContactStatusFXObjectPropertyEdit(ContactStatusFX contactStatusFXObjectPropertyEdit) {
+        this.contactStatusFXObjectPropertyEdit.set(contactStatusFXObjectPropertyEdit);
     }
 
     public ObservableList<ContactStatusFX> getContactStatusFXObservableList() {
