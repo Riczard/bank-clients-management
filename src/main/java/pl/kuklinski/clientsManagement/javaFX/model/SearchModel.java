a8261cf04565a2391dd8bd027f71b01f1efdbcc2
Riczard <riczardkiller@gmail.com>
2019-09-10 12:10:16 +0200

    refactor and delete dialogutils

--- src/main/java/pl/kuklinski/clientsManagement/javaFX/model/SearchModel.java
+++ src/main/java/pl/kuklinski/clientsManagement/javaFX/model/SearchModel.java
@@ -1,17 +1,14 @@
 package pl.kuklinski.clientsManagement.javaFX.model;
 
-import javafx.scene.control.Dialog;
 import pl.kuklinski.clientsManagement.database.dao.ClientDao;
 import pl.kuklinski.clientsManagement.database.models.Client;
 import pl.kuklinski.clientsManagement.javaFX.modelFX.CreditFX;
 import pl.kuklinski.clientsManagement.utils.CSVUtils;
-import pl.kuklinski.clientsManagement.utils.DialogUtils;
 
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.PrintWriter;
 import java.time.LocalDate;
-import java.util.Arrays;
 import java.util.Objects;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -46,27 +43,25 @@ public class SearchModel {
 
     private String[][] searchForAmounts(String... word) throws FileNotFoundException {
         String[][] result = new String[files.length][3];
-            for(int i = 0; i < result.length; i++){
-                File file = files[i];
-                if(file.isFile()) {
-                    result[i][0] = file.getName().replace(".txt","");
-                    String stringFromFile  = CSVUtils.getStringFromFile(file).replaceAll("\\s+","");
-                    result[i][1] = searchAmountByCreditType(stringFromFile, word[0]);
-                    if(word.length > 1 ) {
-                        result[i][2] = searchAmountByCreditType(stringFromFile, word[1]);
-                    }
+        for (int i = 0; i < result.length; i++) {
+            File file = files[i];
+            if (file.isFile()) {
+                result[i][0] = file.getName().replace(".txt", "");
+                String stringFromFile = CSVUtils.getStringFromFile(file).replaceAll("\\s+", "");
+                result[i][1] = searchAmountByCreditType(stringFromFile, word[0]);
+                if (word.length > 1) {
+                    result[i][2] = searchAmountByCreditType(stringFromFile, word[1]);
                 }
             }
+        }
         return result;
     }
 
     public String searchAmountByCreditType(String data, String creditType) {
-        DialogUtils.informationDialog(creditType);
         String regex = String.format(".*%s.*?Maksymalnakwota:(.*?zł)", creditType);
-        DialogUtils.informationDialog(regex);
         Pattern p = Pattern.compile(regex);
         Matcher m = p.matcher(data);
-        if(m.find()) {
+        if (m.find()) {
             return m.group(1);
         }
         return null;
@@ -76,27 +71,27 @@ public class SearchModel {
         PrintWriter printWriter = null;
         try {
             printWriter = new PrintWriter("clickCheck_" + LocalDate.now());
-        }catch (FileNotFoundException e){
+        } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
-        for(String[] singleData : data) {
-            Objects.requireNonNull(printWriter).write(singleData[0]+";" + singleData[1] + ";" + singleData[2] + "\n");
+        for (String[] singleData : data) {
+            Objects.requireNonNull(printWriter).write(singleData[0] + ";" + singleData[1] + ";" + singleData[2] + "\n");
         }
         Objects.requireNonNull(printWriter).close();
     }
 
     public void importToDB(String[][] data) {
         ClientDao clientDao = new ClientDao();
-        for(String[] singleData : data) {
+        for (String[] singleData : data) {
             Client client = clientDao.findByPesel(singleData[peselIndex]);
-            if(client == null) {
+            if (client == null) {
                 client = new Client();
                 client.setPesel(singleData[peselIndex]);
                 client.setClickAmount(singleData[preapprovedIndex]);
                 client.setConsolidationAmount(singleData[consolidationIndex]);
                 client.setVerificationDate(LocalDate.now());
                 clientDao.create(client);
-            }else {
+            } else {
                 client.setClickAmount(singleData[preapprovedIndex]);
                 client.setConsolidationAmount(singleData[consolidationIndex]);
                 client.setVerificationDate(LocalDate.now());
