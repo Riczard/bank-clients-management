465f034065c1caa65c2e7c38ccb026c1564c35ba
Riczard <riczardkiller@gmail.com>
2019-09-10 14:39:48 +0200

    refactor

--- src/main/java/pl/kuklinski/clientsManagement/controllers/ContactStatusController.java
+++ src/main/java/pl/kuklinski/clientsManagement/controllers/ContactStatusController.java
@@ -7,8 +7,8 @@ import javafx.scene.control.MenuItem;
 import javafx.scene.control.TextField;
 import javafx.scene.control.cell.TextFieldListCell;
 import javafx.util.StringConverter;
-import pl.kuklinski.clientsManagement.javaFX.modelFX.ContactStatusFX;
 import pl.kuklinski.clientsManagement.javaFX.model.ContactStatusModel;
+import pl.kuklinski.clientsManagement.javaFX.modelFX.ContactStatusFX;
 
 public class ContactStatusController {
 
@@ -21,11 +21,12 @@ public class ContactStatusController {
     @FXML
     private TextField statusField;
 
-    private ContactStatusModel accStatusModel;
+    private ContactStatusModel contactStatusModel;
 
+    @FXML
     public void initialize() {
-        this.accStatusModel = new ContactStatusModel();
-        this.accStatusModel.init();
+        this.contactStatusModel = new ContactStatusModel();
+        this.contactStatusModel.init();
         bindings();
         bindingsListView();
 
@@ -49,33 +50,33 @@ public class ContactStatusController {
     }
 
     private void bindings() {
-        this.accStatusModel.accountStatusFXObjectPropertyy().get().contactStatusProperty().bind(this.statusField.textProperty());
+        this.contactStatusModel.accountStatusFXObjectPropertyy().get().contactStatusProperty().bind(this.statusField.textProperty());
         this.addButton.disableProperty().bind(this.statusField.textProperty().isEmpty());
         this.deleteStatus.disableProperty().bind(this.statusListView.getSelectionModel().selectedItemProperty().isNull());
     }
 
     private void bindingsListView() {
-        this.statusListView.setItems(this.accStatusModel.getContactStatusFXObservableList());
+        this.statusListView.setItems(this.contactStatusModel.getContactStatusFXObservableList());
         this.statusListView.getSelectionModel().selectedItemProperty().addListener((observable, oldValue, newValue) -> {
-            this.accStatusModel.setAccountStatusFXObjectPropertyEdit(newValue);
+            this.contactStatusModel.setAccountStatusFXObjectPropertyEdit(newValue);
         });
     }
 
-
     @FXML
     public void addAccStatus() {
-        this.accStatusModel.saveStatusInDataBase();
+        this.contactStatusModel.saveStatusInDataBase();
         this.statusField.clear();
     }
 
-
+    @FXML
     public void deleteStatusOnAction() {
-        this.accStatusModel.deleteStatusInDataBase();
+        this.contactStatusModel.deleteStatusInDataBase();
     }
 
+    @FXML
     public void editStatus(ListView.EditEvent<ContactStatusFX> accountStatusFXEditEvent) {
-        this.accStatusModel.getAccountStatusFXObjectPropertyEdit().setContactStatus(accountStatusFXEditEvent.getNewValue().getContactStatus());
-        this.accStatusModel.updateStatusInDB();
+        this.contactStatusModel.getAccountStatusFXObjectPropertyEdit().setContactStatus(accountStatusFXEditEvent.getNewValue().getContactStatus());
+        this.contactStatusModel.updateStatusInDB();
     }
 
 }
