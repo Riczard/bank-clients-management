0bbe07d2846298cc3d7704a7f135c82055eae2f2
Riczard <riczardkiller@gmail.com>
2019-09-12 15:03:55 +0200

    add bindings for fields and add method filterAll

--- src/main/java/pl/kuklinski/clientsManagement/controllers/FiltersController.java
+++ src/main/java/pl/kuklinski/clientsManagement/controllers/FiltersController.java
@@ -2,12 +2,18 @@ package pl.kuklinski.clientsManagement.controllers;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+import pl.kuklinski.clientsManagement.javaFX.model.FilterModel;
 import pl.kuklinski.clientsManagement.javaFX.modelFX.ClientFX;
 
 import java.util.List;
 
 public class FiltersController {
 
+    @FXML
+    private GridPane filtersPane;
+    @FXML
+    private TextField generalFilter;
     @FXML
     private TextField peselFilter;
     @FXML
@@ -23,25 +29,40 @@ public class FiltersController {
 
     private ListClientsController listClientsController;
 
+    private FilterModel filterModel;
+
+    @FXML
+    public void initialize() {
+        this.filterModel = new FilterModel();
+        bindings();
+    }
+
+    private void bindings() {
+        this.filterModel.filterFXObjectProperty().get().generalFilterProperty().bind(this.generalFilter.textProperty());
+        this.filterModel.filterFXObjectProperty().get().peselFilterProperty().bind(this.peselFilter.textProperty());
+        this.filterModel.filterFXObjectProperty().get().surnameFilterProperty().bind(this.surnameFilter.textProperty());
+        this.filterModel.filterFXObjectProperty().get().relationFilterProperty().bind(this.relationFilter.textProperty());
+        this.filterModel.filterFXObjectProperty().get().offerStatusFilterProperty().bind(this.offerStatusFilter.textProperty());
+        this.filterModel.filterFXObjectProperty().get().contactStatusFilterProperty().bind(this.contactStatusFilter.textProperty());
+        this.filterModel.filterFXObjectProperty().get().sourceFilterProperty().bind(this.sourceFilter.textProperty());
+    }
+
     @FXML
     public void showOrHideFilters() {
-        filters.setVisible(!filters.visibleProperty().get());
-        filters.setDisable(!filters.disableProperty().get());
+        filtersPane.setVisible(!filtersPane.visibleProperty().get());
+        filtersPane.setDisable(!filtersPane.disableProperty().get());
     }
 
     @FXML
-    public void filterAll() {
-        filtersController.filterAllClients(this.listClientsModel.getClientFXList());
+    public void filterClients() {
+        List<ClientFX> filteredList = filterModel.filterClients(listClientsController.getClientFxList());
+        listClientsController.setClientFxList(filteredList);
     }
-
 
     @FXML
     public void setListClientsController(ListClientsController listClientsController) {
         this.listClientsController = listClientsController;
     }
 
-    public void filterAllClients(List<ClientFX> clientFXList) {
 
-        listClientsController.setClientFxList(clientFXList);
-    }
 }
