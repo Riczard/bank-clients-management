352ba3d059c806bc510ccb5dcac27dd4bcdb09ff
Riczard <riczardkiller@gmail.com>
2019-09-12 16:55:03 +0200

    set invisible on start and disable filters

--- src/main/java/pl/kuklinski/clientsManagement/javaFX/modelFX/FilterFX.java
+++ src/main/java/pl/kuklinski/clientsManagement/javaFX/modelFX/FilterFX.java
@@ -3,7 +3,6 @@ package pl.kuklinski.clientsManagement.javaFX.modelFX;
 import javafx.beans.property.ObjectProperty;
 import javafx.beans.property.SimpleObjectProperty;
 import javafx.beans.property.SimpleStringProperty;
-import javafx.beans.property.StringProperty;
 
 public class FilterFX {
 
